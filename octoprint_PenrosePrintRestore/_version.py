d612aead3012b75727c8b6951fff03eca85a1a9b
Vijay Raghav Varada <vjvarada@gmail.com>
2026-03-18 11:44:03 +0530

    Fix versioneer: restore Format placeholders in _version.py

--- octoprint_PenrosePrintRestore/_version.py
+++ octoprint_PenrosePrintRestore/_version.py
@@ -23,9 +23,9 @@ def get_keywords():
     # setup.py/versioneer.py will grep for the variable names, so they must
     # each be defined on a line of their own. _version.py will just call
     # get_keywords().
-    git_refnames = " (HEAD -> main)"
-    git_full = "72438727634eb46b17ad3996339a21e8cccc6500"
-    git_date = "2025-09-09 12:33:15 +0530"
+    git_refnames = "$Format:%d$"
+    git_full = "$Format:%H$"
+    git_date = "$Format:%ci$"
     keywords = {"refnames": git_refnames, "full": git_full, "date": git_date}
     return keywords
 
